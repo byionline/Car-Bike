ecbd9bd0585f5c39efd332ca3209a3410c542e83
byionline <10815566+byionline@users.noreply.github.com>
2021-07-08 22:39:39 +0530

    Added Working Steps

--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -4,6 +4,7 @@ import tw from "twin.macro";
 import { Marginer } from "../../components/margin";
 import { NavBar } from "../../components/navbar";
 import { TopSection } from "./topSection";
+import { WorkingSteps } from "./workingSteps";
 const TopContainer = styled.div`
   ${tw` flex flex-col w-full h-full items-center overflow-x-hidden `};
 `;
@@ -13,6 +14,8 @@ export function HomePage() {
       <NavBar />
       <Marginer direction="vertical" margin="3em" />
       <TopSection />
+      <Marginer direction="vertical" margin="8em" />
+      <WorkingSteps />
     </TopContainer>
   );
 }
