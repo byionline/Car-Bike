5fbfb54ca402c68b0e227219e6211a22af164657
byionline <10815566+byionline@users.noreply.github.com>
2021-07-08 15:24:24 +0530

    Added Logo to NavBar

---
+++ src/app/components/logo/index.tsx
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import tw from "twin.macro";
+import LogoImg from "../../../assets/images/logo.jpeg";
+const LogoContainer = styled.div`
+  ${tw` flex items-center `}
+`;
+const LogoText = styled.div`
+  ${tw` text-xl md:text-2xl font-bold text-black m-1 `}
+`;
+const Image = styled.div`
+  ${tw` h-6 md:h-9 `} img {
+    width: auto;
+    height: 100%;
+  }
+`;
+export function Logo() {
+  return (
+    <LogoContainer>
+      <Image>
+        <img src={LogoImg} alt="Logo" />
+      </Image>
+      <LogoText>Car &amp; Bike</LogoText>
+    </LogoContainer>
+  );
+}
--- src/app/containers/HomePage/index.tsx
+++ src/app/containers/HomePage/index.tsx
@@ -1,8 +1,13 @@
 import styled from "styled-components";
 import tw from "twin.macro";
+import { NavBar } from "../../components/navbar";
 const TopContainer = styled.div`
   ${tw` flex flex-col w-full h-full items-center overflow-x-hidden `};
 `;
 export function HomePage() {
-  return <TopContainer>Hello Rajnish</TopContainer>;
+  return (
+    <TopContainer>
+      <NavBar />
+    </TopContainer>
+  );
 }
